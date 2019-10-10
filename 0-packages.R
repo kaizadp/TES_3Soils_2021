81c2f173e44d499b2b9a77242e3021e0af32df56
Kaizad <kaizad.patel@pnnl.gov>
2019-10-10 09:40:13 -0700

    custom ggplot theme

--- 0-packages.R
+++ 0-packages.R
@@ -29,5 +29,20 @@ library(agricolae)
 library(googlesheets)
 library(gsheet)
 
+# create a custom ggplot theme
+theme_kp <- function() {  # this for all the elements common across plots
+  theme_bw() %+replace%
+    theme(legend.position = "top",
+          legend.key = element_rect(size = 3),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          legend.key.size = unit(1.5, 'lines'),
+          plot.title = element_text(hjust = 0.05, size = 14),
+          axis.text = element_text(size = 14, face = "bold", color = "black"),
+          axis.title = element_text(size = 14, face = "bold", color = "black"))
+}
+
+
+
 # DATA_DIR               <- "data/"
 # OUTPUT_DIR		         <- "outputs/"
