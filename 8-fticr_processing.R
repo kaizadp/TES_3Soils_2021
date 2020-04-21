5c21f5e4d25f6614fabedb89bc2080bce6e4119b
Kaizad <kaizad.patel@pnnl.gov>
2020-04-21 11:16:47 -0700

    cleaning. remove PCA script. remove irrelevant, commented code

--- 8-fticr_processing.R
+++ 8-fticr_processing.R
@@ -37,15 +37,6 @@ fticr_soil_meta %>%
                 OC = round(OC,4))->
   fticr_meta_hcoc
 
-# make a subset for relevant columns
-
-
-### OUTPUT
-
-      # merge metadata with sample data
-      # ¿¿¿ do this later instead? YES
-      # fticr_soil = merge(fticr_soil_meta,fticr_soil_data,by = "Mass")
-
 #
 ## step 2: clean and process ----
 fticr_soil_data %>% 
@@ -66,38 +57,6 @@ fticr_soil_data %>%
   fticr_soil_raw_long
 # used to be called:  `fticr_soil_gather2`
 
-
-        ## fticr_soil_data %>% 
-        ##   # melt/gather. transform from wide to long form
-        ##   gather(core, intensity, C1:S25) %>% ## core = name of new categ column, intensity = name of values column, C1:C25 are columns that are collapsed
-        ##   # remove all samples with zero intensity
-        ##   filter(!intensity=="0") %>% 
-        ##   # merge with the core key file
-        ##   left_join(corekey,by = "core") %>% 
-        ##   # merge with hcoc file
-        ##   left_join(fticr_meta_hcoc, by = "Mass") %>% 
-        ##               # since we are using MolForm instead of Mass, we need to average out Mass values in case peaks are repeated 
-        ##               #  group_by(MolForm,Class, core, treatment, site) %>% 
-        ##               #  dplyr::summarize(intensity = mean(intensity)) %>% 
-        ##   ## now we need to filter only those peaks seen in 3 or more replicates
-        ##   # add a column with no. of replicates
-        ##   group_by(Mass,treatment,site) %>% 
-        ##   dplyr::mutate(reps = n()) %>% 
-        ##   # remove peaks seen in < 3 replicates 
-        ##   filter(reps>2) %>% 
-        ##   # remove "unassigned" molecules
-        ##   filter(!Class=="Unassigned")  ->
-        ##   fticr_soil_raw_long2
-        ## 
-        ## fticr_soil_raw_long2 %>% 
-        ##   group_by(Mass, core, site, treatment) %>% 
-        ##   dplyr::mutate(molform_n = n())->tem        p2
-
-
-        ## fticr_soil_raw_long %>% 
-        ##   group_by(Mass,core,site,treatment) %>% 
-        ##   dplyr::summarize(n = n())->temp
-
 ## now create a summary of this
 fticr_soil_raw_long %>% 
   ungroup %>% 
@@ -109,7 +68,6 @@ fticr_soil_raw_long %>%
 
 #
 ### FTICR-SOIL OUTPUT ----
-    # write.csv(fticr_soil_gather2,"fticr_soil_longform.csv")
 
 write.csv(fticr_soil_meta, FTICR_SOIL_META, row.names = FALSE)
 write.csv(fticr_meta_hcoc, FTICR_SOIL_META_HCOC, row.names = FALSE)
@@ -124,12 +82,6 @@ write.csv(fticr_soil_raw_long, FTICR_SOIL_RAW_LONG, row.names = FALSE)
 # the porewater file contains the Mass/Peak metadata as well as sample intensities data
 fticr_porewater = read_csv("data/FTICR_INPUT_SOILPORE.csv.zip")
 corekey = read.csv("data/COREKEY.csv")
-
-# this is the link to the google drive file
-# https://drive.google.com/file/d/1dMjnCnMUYa5XY2ypVjz2HBQKx7E0YJY1/view?usp=sharing
-# use report sn3
-
-# write_csv(fticr_porewater, path = "fticr/fticr_porewater.csv")
 
 #
 ## step 2: clean and process ----
@@ -174,6 +126,7 @@ fticr_porewater %>%
 ### create meta file ----
 ## sample data split by pore size (50 kPa and 1.5 kPa). 
 fticr_porewater %>%
+# exclude C13 peaks  
   filter(C13==0) %>% 
   dplyr::select(-C13) %>% 
   dplyr::select(1:11) %>% 
@@ -186,7 +139,8 @@ fticr_porewater %>%
                 OC = round(O/C,2))->
   fticr_pore_meta
 
-#### calculate molecular formula
+# calculate molecular formula
+# not using molform for further analyses, because it did not change much vs. peaks
 fticr_pore_meta %>% 
   dplyr::select(1:8) %>% 
   dplyr::mutate(mol_C = case_when(C==1 ~ paste("C"),
@@ -214,9 +168,6 @@ fticr_pore_meta %>%
                 MolForm = str_replace_all(MolForm,"NA","")) %>% 
   dplyr::select(Mass,MolForm)->molform_temp
 
-        ## molform_temp%>% 
-        ##   left_join(fticr_pore_meta, by = "Mass")->fticr_pore_meta
-
 
 # create subset for HCOC and class
 fticr_pore_meta %>% 
@@ -231,7 +182,7 @@ fticr_porewater %>%
   melt(id="Mass") %>% 
   dplyr::rename(sample = variable,
                 intensity = value) %>% 
-# remove all peaks with intensity ==0  
+# remove all peaks with intensity==0  
   filter(!intensity==0) %>% 
 # using `sample` column, create columns for tension and core
   dplyr::mutate(tension_temp = substr(sample,start=1,stop=1),
@@ -282,7 +233,6 @@ soil_meta_nosc <- fticr_soil_meta %>%
 soil_nosc <- merge(fticr_soil_long,soil_meta_nosc, by="Mass")
 
 ### pore
-
 pore_meta_nosc <- fticr_pore_meta %>% 
   dplyr::select(Mass,NOSC)
 pore_nosc <- merge(fticr_pore_long,pore_meta_nosc, by="Mass")
@@ -292,157 +242,10 @@ write.csv(soil_nosc,FTICR_SOIL_NOSC,row.names = FALSE)
 write.csv(pore_nosc,FTICR_PORE_NOSC,row.names = FALSE)
 
 
-## b. PCA ----
-library(devtools)
-install_github("vqv/ggbiplot")
-library(ggbiplot)
-library(vegan)
-library("ape")
-
-fticr_pore_raw_long = read.csv(FTICR_PORE_RAW_LONG)# <- "fticr/fticr_pore_longform.csv"
-
-fticr_pore_pca = 
-  fticr_pore_raw_long %>% 
-#  filter(tension=="1.5 kPa") %>% 
-  dplyr::mutate(presence = case_when(intensity>0~1)) %>% 
-  dplyr::select(core,Mass,tension,site,treatment,presence) %>%  
-  spread(Mass,presence)
-
-fticr_pca_num = 
-  fticr_pore_pca %>% 
-  dplyr::select(.,-(1:4)) %>% 
-  replace(.,is.na(.),0)
-
-fticr_pca_grp = 
-  fticr_pore_pca %>% 
-  dplyr::select(.,(1:4)) %>% 
-  dplyr::mutate(row = row_number())
-
-df_f <- fticr_pca_num[,apply(fticr_pca_num, 2, var, na.rm=TRUE) != 0]
-
-pca = prcomp(df_f, scale. = T)
-summary(pca)
-
-ggbiplot(pca, obs.scale = 1, var.scale = 1, 
-         groups = fticr_pca_grp$treatment, ellipse = TRUE, circle = TRUE,
-         var.axes = FALSE)
-
-## TPC method
-
-bray_distance = vegdist(fticr_pca_num, method="euclidean")
-principal_coordinates = pcoa(bray_distance)
-
-pcoa_plot = data.frame(principal_coordinates$vectors[,])
-pcoa_plot_merged = merge(pcoa_plot, fticr_pca_grp, by="row.names")
-
-####### Calculate percent variation explained by PC1, PC2
-
-PC1 <- 100*(principal_coordinates$values$Eigenvalues[1]/sum(principal_coordinates$values$Eigenvalues))
-PC2 <- 100*(principal_coordinates$values$Eigenvalues[2]/sum(principal_coordinates$values$Eigenvalues))
-PC3 <- 100*(principal_coordinates$values$Eigenvalues[3]/sum(principal_coordinates$values$Eigenvalues))
-
-###### Plot PCoA
-
-ggplot(data=pcoa_plot_merged,aes(x=Axis.1,y=Axis.2,color=treatment, shape=site)) + 
-  geom_point(size=4)+
-  facet_grid(.~tension)+
-  #stat_ellipse()+
-  theme_kp()+
-  labs(x = paste("PC1 - Variation Explained", round(PC1,2),"%"), y = paste("PC2 - Variation Explained", round(PC2,2),"%"))
-
-
-
-###### Significance testing
-
-adonis(df_f ~ fticr_pore_pca$site+fticr_pore_pca$treatment+fticr_pore_pca$site:fticr_pore_pca$treatment, method="bray", permutations=100)
-
-## distance matrix
-bray_df  =matrixConvert(bray_distance, colname = c("Var1","Var2","dist"))
-
-bray_df2 = 
-  bray_df %>% 
-  left_join(fticr_pca_grp, by = c("Var1"="row")) %>% 
-  left_join(fticr_pca_grp, by = c("Var2"="row")) %>% 
-#  dplyr::mutate(NAME = paste(site.x,treatment.x,"-",site.y,treatment.y)) %>% 
-# now select only TZSat distances
-  dplyr::mutate(TZSAT = case_when(treatment.x=="time zero saturation" | treatment.y=="time zero saturation"~"time zero saturation")) %>% 
-  filter(!is.na(TZSAT)) %>% 
-  dplyr::mutate(trtx = case_when(!treatment.x=="time zero saturation" ~treatment.x),
-                trty = case_when(!treatment.y=="time zero saturation" ~treatment.y),
-                TRT = paste(trtx,trty),
-                TRT = str_replace_all(TRT,"NA ",""),
-                TRT = str_replace_all(TRT," NA",""),
-                TRT = str_replace_all(TRT,"NA",""))
-
-    
-
-ggplot(bray_df2, aes(x = TRT, y = dist, color = TRT))+
-  geom_boxplot(position = position_dodge(width = 0.7), fill = "white", lwd = 1,fatten = 1, width=0.5)+ # fatten changes thickness of median line, lwd changes thickness of all lines
-  geom_point(position = position_dodge(width = 0.7), alpha=0.1, shape=1, size=2)  
-
-
-bray_summary = melt(as.matrix(bray_distance))
-
-# soil extracts
-fticr_soil_long = read.csv(FTICR_SOIL_RAW_LONG) #<- "fticr/fticr_soil_longform.csv"
-
-fticr_soil_pca = 
-  fticr_soil_long %>% 
-  dplyr::mutate(presence = case_when(intensity>0~1)) %>% 
-  dplyr::select(core,Mass,site,treatment,presence) %>%  
-  spread(Mass,presence)
-
-fticr_soil_pca_num = 
-  fticr_soil_pca %>% 
-  dplyr::select(.,-(1:3)) %>% 
-  replace(.,is.na(.),0)
-
-fticr_soil_pca_grp = 
-  fticr_soil_pca %>% 
-  dplyr::select(.,(1:3)) %>% 
-  dplyr::mutate(row = row_number())
-
-df_f <- fticr_soil_pca_num[,apply(fticr_soil_pca_num, 2, var, na.rm=TRUE) != 0]
-
-pca = prcomp(df_f, scale. = T)
-summary(pca)
-
-ggbiplot(pca, obs.scale = 1, var.scale = 1, 
-         groups = fticr_soil_pca_grp$treatment, ellipse = TRUE, circle = TRUE,
-         var.axes = FALSE)
-ggbiplot(pca, obs.scale = 1, var.scale = 1, 
-         groups = fticr_soil_pca_grp$site, ellipse = TRUE, circle = TRUE,
-         var.axes = FALSE)
-
-
-bray_distance = vegdist(fticr_soil_pca_num, method="euclidean")
-principal_coordinates = pcoa(bray_distance)
-
-pcoa_plot = data.frame(principal_coordinates$vectors[,])
-pcoa_plot_merged = merge(pcoa_plot, fticr_soil_pca_grp, by="row.names")
-
-####### Calculate percent variation explained by PC1, PC2
-
-PC1 <- 100*(principal_coordinates$values$Eigenvalues[1]/sum(principal_coordinates$values$Eigenvalues))
-PC2 <- 100*(principal_coordinates$values$Eigenvalues[2]/sum(principal_coordinates$values$Eigenvalues))
-PC3 <- 100*(principal_coordinates$values$Eigenvalues[3]/sum(principal_coordinates$values$Eigenvalues))
-
-###### Plot PCoA
-
-ggplot(data=pcoa_plot_merged,aes(x=Axis.1,y=Axis.2,color=treatment, shape=site)) + 
-  geom_point(size=4)+
-  #facet_grid(.~site)+
-  #stat_ellipse()+
-  theme_kp()+
-  theme(legend.position = "right")+
-  labs(x = paste("PC1 - Variation Explained", round(PC1,2),"%"), y = paste("PC2 - Variation Explained", round(PC2,2),"%"))
-
-#
-
-### unique to each site ---- pores ----
+## b. unique to each site ---- pores ----
+#### MOVE THIS TO MARKDOWN ----
 fticr_pore_raw_long = read.csv(FTICR_PORE_RAW_LONG)# <- "fticr/fticr_pore_longform.csv"
 fticr_pore_meta = read.csv(FTICR_PORE_META)
-
 
 unique_pore_temp = 
   fticr_pore_raw_long %>% 
@@ -454,17 +257,13 @@ unique_pore_temp =
   dplyr::mutate(reps=sum(presence)) %>% 
   left_join(dplyr::select(fticr_pore_meta, Mass, HC, OC), by = "Mass")
 
-
 unique_pore = 
   unique_pore_temp %>% 
   filter(reps==1)
-  left_join(dplyr::select(fticr_pore_meta, Mass, HC, OC), by = "Mass")
 
 common_pore = 
   unique_pore_temp %>% 
   filter(reps>1) 
-  left_join(dplyr::select(fticr_pore_meta, Mass, HC, OC), by = "Mass")
-
 
 gg_vankrev(unique_pore, aes(x = OC, y = HC, color = site))+facet_wrap(~tension)
 
