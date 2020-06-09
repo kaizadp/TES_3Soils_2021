4630103be3f452a402db0fe77996f628e818de93
Kaizad <kaizad.patel@pnnl.gov>
2020-06-09 10:02:16 -0700

    pore distrib. filter 50-1500 um. clean with dplyr.

--- 6-characterization.R
+++ 6-characterization.R
@@ -95,11 +95,25 @@ write.csv(soil_character_summary2, CHARACTERIZATION, row.names = FALSE)
 pores = read_excel("data/pore_size.xlsx")
 names(pores)
 
+# filter only pores 50-1500 um
+pores_long = 
+  pores %>% 
+  gather(key="site",
+         value = "pore") %>% 
+  filter(pore>50 & pore<=1500)
+
+# create summary table
+pores_summary = 
+  pores_long %>% 
+  group_by(site) %>% 
+  dplyr::summarise(mean_um = mean(pore),
+                   median_um = median(pore))
+
 #creating frequency distribution tables for each site
-cpcrw_pore_scores = pores$CPCRW
-dwp_pore_scores = pores$DWP
-sr_pore_scores = pores$SR
-bins = seq(0,4000, by = 100)
+cpcrw_pore_scores = pores_long %>% filter(site %in% "CPCRW") %>% pull(pore)
+dwp_pore_scores = pores_long %>% filter(site %in% "DWP") %>% pull(pore)
+sr_pore_scores = pores_long %>% filter(site %in% "SR") %>% pull(pore)
+bins = seq(0,1500, by = 100)
 
 cpcrw_scores = cut(cpcrw_pore_scores,bins)
 dwp_scores = cut(dwp_pore_scores,bins)
@@ -107,15 +121,15 @@ sr_scores = cut(sr_pore_scores,bins)
 
 freq_cpcrw = transform(table(cpcrw_scores))
 c = transform(freq_cpcrw,Cum_Freq=cumsum(Freq),Perc_Freq = prop.table(Freq)*100)
-c$scores = seq(0,3999,by = 100)
+c$scores = seq(0,1499,by = 100)
 
 freq_dwp = transform(table(dwp_scores))
 d = transform(freq_dwp,Cum_Freq=cumsum(Freq),Perc_Freq = prop.table(Freq)*100)
-d$scores = seq(0,3999,by = 100)
+d$scores = seq(0,1499,by = 100)
 
 freq_sr = transform(table(sr_scores))
 s = transform(freq_sr,Cum_Freq=cumsum(Freq),Perc_Freq = prop.table(Freq)*100)
-s$scores = seq(0,3999,by = 100)
+s$scores = seq(0,1499,by = 100)
 
 #combining the three sites, two at a time
 #the file contains frequency, cum freq, and % frequency
@@ -128,13 +142,30 @@ names(combined_pore_freq)
 combined_pore_perc_freq = data.frame(combined_pore_freq$scores,
                                      combined_pore_freq$Perc_Freq.x,
                                      combined_pore_freq$Perc_Freq.y,
-                                     combined_pore_freq$Perc_Freq)
-names(combined_pore_perc_freq) = c("pore_size","cpcrw","dwp","sr")
+                                     combined_pore_freq$Perc_Freq,
+                                     combined_pore_freq$cpcrw_scores)
+names(combined_pore_perc_freq) = c("pore_size","cpcrw","dwp","sr","bins")
+
+combined_pore_perc_freq2 = 
+  combined_pore_perc_freq %>% 
+  dplyr::mutate(bins = stri_replace_all_fixed(bins, "(",""),
+                bins = stri_replace_all_fixed(bins, "]","")) %>% 
+  separate(bins, c("low","high"), ",") %>% 
+  dplyr::mutate(low = as.numeric(low),
+                high = as.numeric(high),
+                bins = paste0(low,"-",high))
+
 
 #melting the three sites into a single column
-pores_melt = melt(combined_pore_perc_freq,id = "pore_size")
-names(pores_melt) = c("pore_size","site","freq")
-
+pores_melt = 
+  combined_pore_perc_freq2 %>% 
+  dplyr::select(bins, cpcrw, dwp, sr) %>%
+  dplyr::rename(pore_size_um = bins) %>% 
+  melt(id = "pore_size_um",
+       value.name = "perc_freq",
+       variable.name="site") %>% 
+  dplyr::mutate(perc_freq = round(perc_freq,2))
+  
 ###OUTPUT
 write.csv(pores_melt,PORE_DISTRIBUTION, row.names = FALSE)
 
