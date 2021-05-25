d454ad25020432278b17e86562121cab724b4306
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-05-25 10:26:23 -0700

    update map script. add map, soils images

--- code/map.R
+++ code/map.R
@@ -10,10 +10,18 @@ plot_usmap(color = NA, fill = "grey90", alpha = 1) +
              aes(x = lon.1, y = lat.1, color = site), 
              #color = "black",
              size = 7)+
-#  annotate("text", label = "Secret River (SR)", x = -1686856, y = 411954.3, size=2, hjust="left")+
-#  annotate("text", label = "Caribou Poker Creeks Research Watershed\n(CPCRW)", 
-#           x = -1051020, y = -1842646.7, 
-#           size=2, hjust="left")+
+  annotate("text", label = "Secret River (SR)", fontface = "bold",
+           x = -1606856, y = 411954.3, size=3, hjust="left")+
+  annotate("text", label = "Caribou Poker Creeks Research Watershed\n(CPCRW)", 
+           x = -1001020, y = -1842646.7, fontface = "bold",
+           size=3, hjust="left")+
+  annotate("text", label = "Disney Wilderness Preserve (DWP)", fontface = "bold",
+           x = 300000, y = -1450000.2, size=3, hjust="left")+
+  labs(title = "The Three Soils Project")+
   scale_color_manual(values = PNWColors::pnw_palette("Bay", 3))+
-  theme(legend.position = "none")+
+  theme(legend.position = "none",
+        plot.title = element_text(face = "bold",
+                                  hjust = 0.5, size = 15))+
   NULL
+
+ggsave("map.png", width = 5.5, height = 4.5)
